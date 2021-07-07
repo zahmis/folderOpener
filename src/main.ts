f92581b77d6656dca96af5fa0585ddfccceb56eb
zahmis <meda829ratteru86@gmail.com>
2021-07-07 15:26:09 +0900

    add hot reload npm

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import os from 'os';
 import path from 'path';
+import * as isDev from 'electron-is-dev';
+
 import { app, BrowserWindow, session } from 'electron';
 
 const extPath =
@@ -7,49 +9,24 @@ const extPath =
     ? '/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.13.2_0'
     : '/AppData/Local/Google/Chrome/User Data/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.13.2_0';
 
-/**
- * BrowserWindowインスタンスを作成する関数
- */
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     webPreferences: {
-      /**
-       * BrowserWindowインスタンス（レンダラープロセス）では
-       * Node.jsの機能を無効化する（electron@8以降でデフォルト）
-       */
       nodeIntegration: false,
-      /**
-       * メインプロセスとレンダラープロセスとの間で
-       * コンテキストを共有しない (electron@12以降でデフォルト)
-       */
       contextIsolation: true,
-      /**
-       * Preloadスクリプト
-       * webpack.config.js で 'node.__dirname: false' を
-       * 指定していればパスを取得できる
-       */
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
-  // 開発時にはデベロッパーツールを開く
   if (process.env.NODE_ENV === 'development') {
     mainWindow.webContents.openDevTools({ mode: 'detach' });
   }
 
-  // レンダラープロセスをロード
   mainWindow.loadFile('dist/index.html');
 };
 
-/**
- * アプリを起動する準備が完了したら BrowserWindow インスタンスを作成し、
- * レンダラープロセス（index.htmlとそこから呼ばれるスクリプト）を
- * ロードする
- */
 app.whenReady().then(async () => {
-  /**
-   * 開発時には React Developer Tools をロードする
-   */
+
   if (process.env.NODE_ENV === 'development') {
     await session.defaultSession
       .loadExtension(path.join(os.homedir(), extPath), {
@@ -59,9 +36,7 @@ app.whenReady().then(async () => {
       .catch((err) => console.log(err));
   }
 
-  // BrowserWindow インスタンスを作成
   createWindow();
 });
 
-// すべてのウィンドウが閉じられたらアプリを終了する
 app.once('window-all-closed', () => app.quit());
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App: React.FC = () => <h1>folder opner app</h1>;
+const App: React.FC = () => <h1 className="test-center">folder opner app</h1>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
