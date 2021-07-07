11e2687382235dc736685a5c0c52dd7458428ff2
zahmis <meda829ratteru86@gmail.com>
2021-07-07 16:06:23 +0900

    create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, {VFC} from 'react';
+
+const App: React.VFC = () => {
+  return (  
+    <h1 className="text-center">folder opner app</h1>
+  )
+}
+
+
+export default App;
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
-const App: React.FC = () => <h1 className="test-center">folder opner app</h1>;
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
