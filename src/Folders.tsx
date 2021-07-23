477d60f4775bf233d19d24a1deb76bfec642daaa
zahmis <meda829ratteru86@gmail.com>
2021-07-24 01:25:50 +0900

    fix

--- src/Folders.tsx
+++ src/Folders.tsx
@@ -1,27 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Folders = () => {
 
-  function clickHandler(){
-    console.log("hoges")
+  const [target, setTarget] = useState(1)
+
+  function clickHandler(target:number){
+    setTarget(target)
+    console.log(target)
   }
 
   return (  
-    
+    <>
     <div className="d-flex flex-column w-25">
-      <button onClick={clickHandler}>
-        <img src="../src/assets/images/cupmen-1.png" />
-      </button>
-      <button>
-      <img src="../src/assets/images/cupmen-2.png" />
-
-      </button>
-      {/* <img src="../src/assets/images/cupmen-2.png" />
-      <img src="../src/assets/images/cupmen-3.png" />
-      <img src="../src/assets/images/cupmen-4.png" /> */}
-
-
+      <button onClick={()=>clickHandler(1)}><img src="../src/assets/images/cupmen-1.png" /></button>
+      <button onClick={()=>clickHandler(2)}><img src="../src/assets/images/cupmen-2.png" /></button>
+      <button onClick={()=>clickHandler(3)}><img src="../src/assets/images/cupmen-3.png" /></button>
+      <button onClick={()=>clickHandler(4)}><img src="../src/assets/images/cupmen-4.png" /></button>
     </div>
+    <div>
+      <img src="../src/assets/images/cupmen-1.png"/>
+    </div>
+    </>
   )
 }
 
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,45 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App'
-// import NudleFolder from './NudleFolder'
-
 import {HashRouter,Link,Route,Switch} from "react-router-dom";
-import Home from './Home';
-// import Folders from './Folders';
+
+import Home from './Home'
+import Folders from './Folders'
+
 
 const App: React.VFC = () => {
 
-    return (
-      <HashRouter>
-      <div className="App">
-        <div className="menu">
-          <Link to="/"><button className="btn btn-primary">Home</button></Link>
-          <Link to="/folder"><button className="btn btn-primary">Folders</button></Link>
-        </div>
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/folder" component={Folders}/>
-        </Switch>     
+  return (
+    <>
+    <HashRouter>
+    <div className="App">
+      <div className="menu">
+        <Link to="/"><button className="btn btn-primary">Home</button></Link>
+        <Link to="/folder"><button className="btn btn-primary">Folders</button></Link>
       </div>
-      </HashRouter>    
-    );
-  }
-
-
-  const clickHandler = () => {
-      console.log('foo')
-  }
-
-  const Folders = () => {
-
-    console.log('1')
-  
-    return (  
-      
-      <div className="d-flex flex-column w-25">
-          <button className="button" onClick={()=>clickHandler()}>hoge</button>
-      </div>
-    )
-  }
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/folder" component={Folders}/>
+      </Switch>     
+    </div>
+    </HashRouter>    
+    </>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
