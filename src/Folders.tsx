35af18bf74bb824a0f596dd311b1ca830740c6f2
zahmis <meda829ratteru86@gmail.com>
2021-07-24 00:06:40 +0900

    fix

--- src/Folders.tsx
+++ src/Folders.tsx
@@ -1,44 +1,25 @@
 import React from 'react';
-import ImageGallery from 'react-image-gallery';
-
-
-
-const images = [
-  {
-    original: '/Users/sy/imageOpener/src/assets/images/cupmen-1.png',
-    thumbnail: '/Users/sy/imageOpener/src/assets/images/cupmen-1.png',
-    thumbnailTitle: "cupmen-1"
-  },
-  {
-    original: '/Users/sy/imageOpener/src/assets/images/cupmen-2.png',
-    thumbnail: '/Users/sy/imageOpener/src/assets/images/cupmen-2.png',
-    thumbnailTitle: "cupmen-2"
-
-  },
-  {
-    original: '/Users/sy/imageOpener/src/assets/images/cupmen-3.png',
-    thumbnail: '/Users/sy/imageOpener/src/assets/images/cupmen-3.png',
-    thumbnailTitle: "cupmen-3"
-  },
-  {
-    original: '/Users/sy/imageOpener/src/assets/images/cupmen-4.png',
-    thumbnail: '/Users/sy/imageOpener/src/assets/images/cupmen-4.png',
-    originalTitle: "cupmen-4"
-
-  }
-]
-
 
 const Folders = () => {
 
+  function clickHandler(){
+    console.log("hoges")
+  }
+
   return (  
-    <div className="text-center">
-      <ImageGallery 
-        items={images} 
-        thumbnailPosition='top'
-        showIndex={true}
-      />
-      {/* <button className="btn btn-primary" onClick={() => handleClick()}>openfolder</button> */}
+    
+    <div className="d-flex flex-column w-25">
+      <button onClick={clickHandler}>
+        <img src="../src/assets/images/cupmen-1.png" />
+      </button>
+      <button>
+      <img src="../src/assets/images/cupmen-2.png" />
+
+      </button>
+      {/* <img src="../src/assets/images/cupmen-2.png" />
+      <img src="../src/assets/images/cupmen-3.png" />
+      <img src="../src/assets/images/cupmen-4.png" /> */}
+
 
     </div>
   )
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
-import NudleFolder from './NudleFolder'
+// import App from './App'
+// import NudleFolder from './NudleFolder'
+
+import {HashRouter,Link,Route,Switch} from "react-router-dom";
+import Home from './Home';
+// import Folders from './Folders';
+
+const App: React.VFC = () => {
+
+    return (
+      <HashRouter>
+      <div className="App">
+        <div className="menu">
+          <Link to="/"><button className="btn btn-primary">Home</button></Link>
+          <Link to="/folder"><button className="btn btn-primary">Folders</button></Link>
+        </div>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/folder" component={Folders}/>
+        </Switch>     
+      </div>
+      </HashRouter>    
+    );
+  }
+
+
+  const clickHandler = () => {
+      console.log('foo')
+  }
+
+  const Folders = () => {
+
+    console.log('1')
+  
+    return (  
+      
+      <div className="d-flex flex-column w-25">
+          <button className="button" onClick={()=>clickHandler()}>hoge</button>
+      </div>
+    )
+  }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<NudleFolder />, document.getElementById('nudle-folder'));
