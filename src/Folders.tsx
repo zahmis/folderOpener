7b56bbcfb120bf3acabcd3237d0494ebd81dbc03
zahmis <meda829ratteru86@gmail.com>
2021-07-23 16:22:10 +0900

    fix

--- src/Folders.tsx
+++ src/Folders.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 const Folders = () => {
 
-  function handleClick(){
-    console.log("hoge")
-  }
-  console.log('hoge1')
+  // function handleClick(){
+  //   console.log("hoge")
+  // }
   
   return (  
     <div className="text-center">
-      <button className="btn btn-primary" onClick={() => handleClick()}>openfolder</button>
+      {/* <button className="btn btn-primary" onClick={() => handleClick()}>openfolder</button> */}
+      
     </div>
   )
 }
