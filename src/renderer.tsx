5f938408256affa204dd451c28dd96514482d08d
zahmis <meda829ratteru86@gmail.com>
2021-07-24 02:40:34 +0900

    clean

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter,Link,Route,Switch} from "react-router-dom";
+import { HashRouter, Link, Route, Switch } from "react-router-dom";
 
 import Home from './Home'
 import Folders from './Folders'
 
 
 const App: React.VFC = () => {
-
   return (
     <>
-    <HashRouter>
-    <div className="App">
-      <div className="menu">
-        <Link to="/"><button className="btn btn-primary">Home</button></Link>
-        <Link to="/folder"><button className="btn btn-primary">Folders</button></Link>
+      <HashRouter>
+      <div className="App">
+        <div className="menu">
+          <Link to="/"><button className="btn btn-primary">Home</button></Link>
+          <Link to="/folder"><button className="btn btn-primary">Folders</button></Link>
+        </div>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/folder" component={Folders}/>
+        </Switch>     
       </div>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/folder" component={Folders}/>
-      </Switch>     
-    </div>
-    </HashRouter>    
+      </HashRouter>    
     </>
   );
 }
